f61f366f796866aaf160c3bbb58ba5125a6a2515
Faizan vahora <developer@Faizan04.onmicrosoft.com>
2024-11-29 11:50:16 +0530

    Add Api Authetication

--- GuessMaster.API/Program.cs
+++ GuessMaster.API/Program.cs
@@ -1,3 +1,4 @@
+using GuessMaster.API.Middleware;
 using GuessMaster.Data.Data;
 using GuessMaster.Repository;
 using GuessMaster.Service;
@@ -14,20 +15,19 @@ builder.Services.AddSwaggerGen();
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-var app = builder.Build();
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 builder.Services.AddScoped<IServiceManager, ServiceManager>();
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
-
+app.UseMiddleware<ApiKeyMiddleware>();
 app.MapControllers();
 
 app.Run();
